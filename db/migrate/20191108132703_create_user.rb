4d7bffbff73fad83dcf42e56858ab3e6ce591959
tsukrus <bereklamuman@gmail.com>
2019-11-08 21:12:27 +0200

    Creating app

---
+++ db/migrate/20191108132703_create_user.rb
@@ -0,0 +1,9 @@
+class CreateUser < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.datetime :created_at
+    end
+  end
+end
